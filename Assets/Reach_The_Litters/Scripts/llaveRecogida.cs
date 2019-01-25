f21a0346607789ed97ff44ffb7f5f8f4043978c8
elleminati <olga.elerye@gmail.com>
2019-01-25 19:07:17 +0100

    #8 Programar Recogida llaves
    Cuando Maddy("Player") o el huevo("Huevo") pasan por la llave desaparecen y se suman 1 en el Gamecontroller Script!!

---
+++ Assets/Reach_The_Litters/Scripts/llaveRecogida.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class llaveRecogida : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //mostramos quien ha tocado el collider de tipo trigger
+        Debug.Log(col.gameObject.name);
+
+        if (col.gameObject.tag == "Huevo" || col.gameObject.tag == "Player")
+
+        {
+            GameController.llaves++;
+            Debug.Log("EUREKA! Ya tengo "+ GameController.llaves );
+            Destroy(gameObject);
+        }
+    }
+
+}
