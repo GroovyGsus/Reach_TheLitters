a821e51cf932c65a8f1bd8c53d1830783bebf97e
elleminati <olga.elerye@gmail.com>
2019-01-25 18:00:37 +0100

    #5 Programar explosion globo

--- Assets/Reach_The_Litters/Scripts/PinchaGlobos.cs
+++ Assets/Reach_The_Litters/Scripts/PinchaGlobos.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PinchaGlobos : MonoBehaviour
+
+
 {
+    public Animator anim;
+    public Rigidbody2D rbGlobo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbGlobo = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -18,6 +25,10 @@ public class PinchaGlobos : MonoBehaviour
 
     void OnMouseDown()
     {
-        Destroy(gameObject);
+       // rbGlobo.isKinematic = true;
+       // rbGlobo.velocity = Vector2.zero;
+        anim.SetTrigger("Pinchar");
+        Destroy(gameObject,1f);
+
     }
 }
