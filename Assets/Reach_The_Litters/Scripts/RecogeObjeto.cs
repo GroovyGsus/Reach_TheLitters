e0ab4975e9ce996ea68a7bac3c880f8a227c7c6a
GroovyGsus <jesuspalomogranada@gmail.com>
2019-01-25 19:50:28 +0100

    #9 Hacer que Tuit agarre objetos cuando los toque
    Cuando lo vuelves a pinchar lo suelta, solo coge el huevo, piedras y a Maddy

---
+++ Assets/Reach_The_Litters/Scripts/RecogeObjeto.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogeObjeto : MonoBehaviour
+{
+    Rigidbody2D rbObjeto;
+    GameObject objeto;
+    bool llevaObjeto = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Huevo") || col.gameObject.CompareTag("Piedras") || col.gameObject.CompareTag("Player"))
+        {
+
+            llevaObjeto = true;
+            col.transform.parent = transform;
+            objeto = col.gameObject;
+            rbObjeto = col.gameObject.GetComponent<Rigidbody2D>();
+            rbObjeto.isKinematic = true;
+
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (llevaObjeto)
+        {
+            rbObjeto.isKinematic = false;
+            objeto.transform.parent = null;
+            llevaObjeto = false;
+
+        }
+    }
+}
+
