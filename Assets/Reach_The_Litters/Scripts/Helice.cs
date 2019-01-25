e7fe26489e0cac0c58bc7fa8d8a2a7a878cd6ff5
Mario <mario7.yt@gmail.com>
2019-01-25 20:34:44 +0100

    #15 Programar fuerza hélice (NO terminado)
    he empezado la programación pero está a medias

---
+++ Assets/Reach_The_Litters/Scripts/Helice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helice : MonoBehaviour
+{
+
+    PointEffector2D poi;
+    public float tiempo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poi = GetComponent<PointEffector2D>();
+
+        poi.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            poi.enabled = true;
+            StartCoroutine(PararMotor());
+        }
+    }
+
+    IEnumerator PararMotor()
+    {
+        yield return new WaitForSeconds(tiempo);
+        poi.enabled = false;
+    }
+}
