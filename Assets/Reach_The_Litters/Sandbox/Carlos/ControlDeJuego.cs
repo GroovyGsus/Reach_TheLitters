63a01c47dd524ed72bbdb4f87bf094f0681f7ada
monkey60 <carlosbalilla@outlook.com>
2019-01-28 20:56:33 +0100

    #85 crear boton pausa
    Viva Escocia¡¡¡¡¡¡¡¡

---
+++ Assets/Reach_The_Litters/Sandbox/Carlos/ControlDeJuego.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDeJuego : MonoBehaviour
+{
+    bool pulsas = false;
+    // Start is called before the first frame update
+    public void SiPulsas()
+    {
+        pulsas = !pulsas;
+        if (pulsas == true)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
