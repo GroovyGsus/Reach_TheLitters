951ba7f21e9a7c4157777aeeaf339ce0b29ea706
Mario <mario7.yt@gmail.com>
2019-01-28 20:38:41 +0100

    #15 Programar fuerza helice
    Acabado el script de la helice

--- Assets/Reach_The_Litters/Scripts/Helice.cs
+++ Assets/Reach_The_Litters/Scripts/Helice.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Helice : MonoBehaviour
 {
-
+    //Declaramos el point effector (que generará la fuerza), una variable tiempo y otra de velocidad máxima
     PointEffector2D poi;
     public float tiempo;
+    public float velMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Accedemos al point effector y lo ponemos falso de base
         poi = GetComponent<PointEffector2D>();
 
         poi.enabled = false;
@@ -19,16 +21,28 @@ public class Helice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            poi.enabled = true;
-            StartCoroutine(PararMotor());
-        }
+        
     }
 
+    //Creamos una corrutina, a la que llamaremos después para que cuando pase el "tiempo" deje de echar aire
     IEnumerator PararMotor()
     {
+        //Devolvemos el tiempo que esta echando aire y volvemos el point effector a falso para que pare
         yield return new WaitForSeconds(tiempo);
         poi.enabled = false;
+    }
+
+    
+    private void OnMouseDown()
+    {
+        poi.enabled = true;            //Aqui cuando pulsemos el ventilador activamos el point effector
+        StartCoroutine(PararMotor());  //Llamamos a la corrutina para que cuando pase el tiempo pare
+        
+        //mientras la fuerza del point effector sea menor que la máxima, le vamos sumando 1 con cada pulsación, cuando llega a la max se para
+        if (poi.forceMagnitude < velMax)   
+        {
+            poi.forceMagnitude = poi.forceMagnitude + 1;
+        }
+        
     }
 }
