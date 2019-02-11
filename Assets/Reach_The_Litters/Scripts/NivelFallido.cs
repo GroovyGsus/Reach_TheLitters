b761985344d9b7152176de8222260bd4ec0ba9ba
pablojpf <pablojpf98@gmail.com>
2019-02-11 21:00:52 +0100

    #79 Programar animacion nivel fallido

--- Assets/Reach_The_Litters/Scripts/NivelFallido.cs
+++ Assets/Reach_The_Litters/Scripts/NivelFallido.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class NivelFallido : MonoBehaviour
 {
+    GameObject panelNivelFallido;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelNivelFallido = GameObject.Find("Panel_nivel_fallido");
+        if(panelNivelFallido == null)
+        {
+            Debug.LogError("Hace falta tener el objeto Panel_nivel_fallido en la escena", gameObject);
+        }
+        else
+        {
+            anim = panelNivelFallido.GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +30,7 @@ public class NivelFallido : MonoBehaviour
     {
         if (other.gameObject.tag == "Area_Juego")
         {
-         
+            anim.SetTrigger("activar");
            
         }
 
