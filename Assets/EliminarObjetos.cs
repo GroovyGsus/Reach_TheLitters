846ce86937184f31bf518a3f37529bd30d02527e
Groovy_Gsus <jesuspalomogranada@gmail.com>
2019-02-15 18:23:57 +0100

    #94 Programar que los objectos se destruyan cuando pasen el area juego

---
+++ Assets/EliminarObjetos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliminarObjetos : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       if (other.gameObject.tag != "Huevo" && other.gameObject.tag !="Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
