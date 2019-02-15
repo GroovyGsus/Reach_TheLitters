d32df40c3cee7d799eea0a98c832089ab222142e
pablojpf <pablojpf98@gmail.com>
2019-02-15 17:47:36 +0100

    Mundo 1 Nivel 1 UI

--- Assets/Reach_The_Litters/Scripts/llaveRecogida.cs
+++ Assets/Reach_The_Litters/Scripts/llaveRecogida.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class llaveRecogida : MonoBehaviour
 {
+    public GameObject gameController;
+
+    private void Start()
+    {
+
+        gameController = GameObject.FindGameObjectWithTag("GameController");
     
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         //mostramos quien ha tocado el collider de tipo trigger
@@ -13,8 +20,15 @@ public class llaveRecogida : MonoBehaviour
         if (col.gameObject.tag == "Huevo" || col.gameObject.tag == "Player")
 
         {
-            GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
-            gameController.GetComponent<GameController>().SonidoCogerLlave();
+            if(gameController != null)
+            {
+                gameController.GetComponent<GameController>().SonidoCogerLlave();
+            }
+            else
+            {
+                Debug.LogError("No encuentro el GameController por su tag");
+            }
+            
 
             GameController.llaves++;
             Debug.Log("EUREKA! Ya tengo "+ GameController.llaves );
