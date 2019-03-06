0081d3e6517e4dd018d51543b9cda51d7fdbaf4c
monkey60 <carlosbalilla@outlook.com>
2019-03-06 20:46:32 +0100

    #115 Slider del demonio arreglado

---
+++ Assets/Reach_The_Litters/Scripts/CargarAudioSourceSlideOpciones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargarAudioSourceSlideOpciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Slider>().onValueChanged.AddListener(UpdateAudio);
+    }
+
+    public void UpdateAudio(float value)
+    {
+        GameObject.Find("MusicaFondo").GetComponent<AudioSource>().volume = value;
+    }
+}
